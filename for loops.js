8bb50692c9003df0778bff8ce83525d9504f354e
Yash Raj <136958320+ykingraj@users.noreply.github.com>
2024-05-16 10:00:56 +0000

    n

--- for loops.js
+++ for loops.js
@@ -2,8 +2,8 @@
 for (statement1 ; statement 2; statement3){
     body of for loop
 }
-*/
 
 for (let i = 1; i <= 100; i++){
     console.log(i)
 }
+*/ 
---
+++ indexing.js
@@ -0,0 +1,6 @@
+let x = "abcd123$@#!"
+let y = [1,22,44,67]
+console.log(y[2])
+
+console.log(y.length)
+
